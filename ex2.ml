86d18e1d12fae7c96a92896cbf7e296aab580625
nmax0 <maxlbnew@gmail.com>
2025-09-11 12:30:43 +0200

    Update ex2.ml

--- ex2.ml
+++ ex2.ml
@@ -1,3 +1,8 @@
+let myst x =
+let sqf = sqrt (float_of_int x) in
+let sqi = float_of_int (int_of_float sqf) in
+if sqf = sqi then "1" else "0" ;;
+
 let top x =
 let x1 = x / 100 in
 let x2 = x / 10 mod 10 in
@@ -11,13 +16,8 @@ let sum = (mid * mid + max * max) in
 let m = myst sum in
 if m = "1" then "1" else "0" ;;
 
-let myst x =
-let sqf = sqrt (float_of_int x) in
-let sqi = float_of_int (int_of_float sqf) in
-if sqf = sqi then "1" else "0" ;;
-
 let main x =
 if (float_of_int x /. 1000. > 1.) || (float_of_int x /. 10. < 10.) then invalid_arg "3 digit num is needed" 
 else (myst x, top x) ;;
 
-(* code = 100, 900 *)
+(* code = 100, 400, 900 *)
