c630914a5fcec57ee57659c41d89fe8b0dc4327b
nmax0 <maxlbnew@gmail.com>
2025-09-11 20:56:51 +0200

    Update ex4.ml

--- ex4.ml
+++ ex4.ml
@@ -5,6 +5,6 @@ let c = b * 1000 + b in
 let d = c / 7 - 7 * (c mod 7) in
 let e = d / 11 - 11 * (d mod 11) in
 let f = e / 13 - 13 * (e mod 13) in
-if f = 555 then true else false ;;
+f = 555 ;;
 
 (* code = 5555 *)
