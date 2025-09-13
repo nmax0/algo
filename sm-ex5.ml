9e56fcd321a0da8dcfdb7bba109cf534fcd8dfda
nmax0 <maxlbnew@gmail.com>
2025-09-13 11:20:19 +0200

    Update sm-ex5.ml

--- sm-ex5.ml
+++ sm-ex5.ml
@@ -1,11 +1,11 @@
-let dmax x = (* non leap years *)
+let dmax x =
   match x with
   | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
   | 4 | 6 | 9 | 11 -> 30
   | 2 -> 28
   | _ -> failwith "not a valid month";;
 
-let int_of_month x =
+let int_of_month x = (* January -> 1 *)
   match x with
   | "January" -> 1
   | "February" -> 2
@@ -21,7 +21,7 @@ let int_of_month x =
   | "December" -> 12
   | _ -> failwith "not a valid month";;
 
-let month_of_int x =
+let month_of_int x = (* 1 -> January *)
   match x with
   | 1 -> "January"
   | 2 -> "February"
@@ -35,11 +35,14 @@ let month_of_int x =
   | 10 -> "October"
   | 11 -> "November"
   | 12 -> "December"
-  | _ -> failwith "not a valid month";;
+  | _ -> failwith "not a valid month" ;;
+
+let add_zero x = (* 9 -> 09 *)
+  if x < 10 then "0" ^ string_of_int x else string_of_int x ;;
 
 (* level 1 *) 
 
-let tmrw_int d m y =
+let tmrw_int d m y = (* format : 5 12 2025 *)
   let dmax = dmax m in
   if d > dmax then failwith "not a valid day";
   if (d+1) > dmax then
@@ -51,27 +54,22 @@ let tmrw_int d m y =
 (* level 2 *)
 
 let tmrw_str s =
-  if String.length s = 10 then (* format : "00/00/0000" *)
+  if String.length s = 10 then (* format : "05/12/2025" *)
     let d1 = int_of_string (String.sub s 0 2) in (* unpacked day val *)
     let m1 = int_of_string (String.sub s 3 2) in (* unpacked month val *)
     let y1 = int_of_string (String.sub s 6 4) in (* unpacked year val *)
     let d2, m2, y2 = tmrw_int d1 m1 y1 in (* compute tmrw's date *)
-    let d3 = if d2 < 10 then "0" ^ string_of_int d2 else string_of_int d2 in (* 9 -> 09 *)
-    let m3 = if m2 < 10 then "0" ^ string_of_int m2 else string_of_int m2 in (* 9 -> 09 *)
-    d3 ^ "/" ^ m3 ^ "/" ^ string_of_int y2 (* concatenation *)
-  else (* format : "Month day, year" *)
-    let len = String.length s in
-    let month_len = len-9 in
+    add_zero d2 ^ "/" ^ add_zero m2 ^ "/" ^ string_of_int y2 (* concatenation *)
+  else (* format : "December 05, 2025" *)
+    let month_len = (String.length s)-9 in
     let m1 = int_of_month (String.sub s 0 (month_len)) in (* unpacked month val *)
     let d1 = int_of_string (String.sub s (month_len+1) 2) in (* unpacked day val *)
     let y1 = int_of_string (String.sub s (month_len+5) 4) in (* unpacked year val *)
     let d2, m2, y2 = tmrw_int d1 m1 y1 in (* comute tmrw's date *)
-    let d3 = if d2 < 10 then "0" ^ string_of_int d2 else string_of_int d2 in (* 9 -> 09 *)
-    let m3 = month_of_int m2 in (* 1 -> January *)
-    m3 ^ " " ^ d3 ^ ", " ^ string_of_int y2 ;; (* concatenation *)
+    month_of_int m2 ^ " " ^ add_zero d2 ^ ", " ^ string_of_int y2 ;; (* concatenation *)
 
 (*
-tmrw_int 25 12 2025 ;;
-tmrw_str "25/12/2025" ;;
-tmrw_str "December 25, 2025" ;;
+tmrw_int 5 12 2025 ;;
+tmrw_str "05/12/2025" ;;
+tmrw_str "December 05, 2025" ;;
 *)
