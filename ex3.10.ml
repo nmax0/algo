8b7217407d614b098fabe2a1ca17101c769c97c6
nmax0 <maxlbnew@gmail.com>
2025-09-15 21:52:15 +0200

    Update ex3.10.ml

--- ex3.10.ml
+++ ex3.10.ml
@@ -1,4 +1,4 @@
-let time_dif (a1, b1, c1, d1) (a2, b2, c2, d2) =
+let time_dif (a1, b1, c1, d1) (a2, b2, c2, d2) = (* time difference between 2 longitude coordinates. format : a° b' c" *)
   let s1 = (a1*240 + b1*4 + c1/15) and s2 = (a2*240 + b2*4 + c2/15) in
   let s1 = if d1 = 'W' then -s1 else s1 and s2 = if d2 = 'W' then -s2 else s2 in
   if s1 > s2 then
@@ -9,8 +9,3 @@ let time_dif (a1, b1, c1, d1) (a2, b2, c2, d2) =
     let dif = s2 - s1 in
     let dh, dm, ds = dif / 3600, dif mod 3600 / 60, dif mod 60 in
     "later : " ^ string_of_int dh ^ "h " ^ string_of_int dm ^ "m " ^ string_of_int ds ^ "s" ;;
-
-(* 
-time_dif (3, 12, 15, 'E') (13, 10, 30, 'W') ;; 
-time_dif (0, 1, 4, 'W') (2, 0, 5, 'E') ;;
-*)
