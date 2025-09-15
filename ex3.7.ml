0879318417e5f1358ccfdaf564adc2a80cd9748b
nmax0 <maxlbnew@gmail.com>
2025-09-15 21:49:31 +0200

    Update ex3.7.ml

--- ex3.7.ml
+++ ex3.7.ml
@@ -1,20 +1,15 @@
-let rate w t =match t with
-  | "eco" -> (match w with
-      | w when w < 500 -> 3.4
-      | w when w < 1000 -> 4.6
-      | w when w < 2000 -> 5.1
-      | w when w < 3000 -> 6.9
-      | _ -> failwith "too heavy")
-  | "standard" -> (match w with
-      | w when w < 500 -> 4.6
-      | w when w < 1000 -> 5.9
-      | w when w < 2000 -> 6.5
-      | w when w < 3000 -> 7.2
-      | _ -> failwith "too heavy")
-  | "express" -> (match w with
-      | w when w < 500 -> 9.1
-      | w when w < 1000 -> 11.
-      | w when w < 2000 -> 13.5
-      | w when w < 3000 -> 14.2
-      | _ -> failwith "too heavy")
-  | _ -> failwith "unknow code" ;;
+let ese w = match w with
+  | w when w < 0 -> invalid_arg "weight cant be a negative number"
+  | w when w < 500 -> (3.4, 4.6, 9.1)
+  | w when w < 1000 -> (4.6, 5.9, 11.)
+  | w when w < 2000 -> (5.1, 6.5, 13.5)
+  | w when w < 3000 -> (6.9, 7.2, 14.2)
+  | _ -> failwith "too heavy" ;;
+
+let rate w t =
+  match ese w with
+  | (ec, st, ex) -> match t with
+    | "eco" -> ec
+    | "standard" -> st
+    | "express" -> ex
+    | _ -> failwith "unknow code" ;;
