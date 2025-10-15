eba734de5d5f0a9ab202d9fb4c7734bb6afe9460
nmax0 <maxlbnew@gmail.com>
2025-10-16 00:04:47 +0200

    6.1 update. x.x
    Updated comments for clarity and consistency.

--- list6.ml
+++ list6.ml
@@ -1,8 +1,9 @@
 (* Small Exercices *)
 
-(* 6.1 : gcd without prime factors *)
+(* 6.1 : gcd *)
+(* without prime factors *)
 let rec gcd a b =
-  if a < b then gcd b a
-  else
-    let r = a mod b in
-    if r = 0 then b else gcd b r ;;
+  let r = a mod b in
+  if r = 0 then b else gcd b r ;;
+(* with prime factors *)
+(* x.x *)
