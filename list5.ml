4242a1b69bf67703d0528edc0986d36844853846
nmax0 <maxlbnew@gmail.com>
2025-10-15 23:44:13 +0200

    add of 5.2 (v1, v2)

--- list5.ml
+++ list5.ml
@@ -8,3 +8,17 @@ let rec assoc k l =
       else if key > k then failwith "not found"
       else assoc k tl
   | [] -> failwith "not found" ;;
+
+(* 5.2 : flat a linked list into a single list *)
+(* with '@' *)
+let rec flatten l =
+  match l with
+  | e::l -> e@flatten l
+  | [] -> [] ;;
+(* without '@'.. it just doesnt work except if every list contains a single element *)
+let rec flatten l =
+  match l with
+  | e::l -> begin match e with
+      | e::l' -> e::flatten l
+      | _ -> [] end
+  | _ -> [] ;;
